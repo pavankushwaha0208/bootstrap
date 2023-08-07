47f3df684b50e4d8206e5b6a7dcf8478537e60db
pavankushwaha0208 <pavankush0208@gmail.com>
2023-08-07 11:54:54 +0530

    jg

--- js/two.js
+++ js/two.js
@@ -1,1 +1,9 @@
-
+// 
+let xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    document.querySelector("div").innerHTML = this.responseText;
+  }
+};
+xhttp.open("GET", "https://jsonplaceholder.typicode.com/posts");
+xhttp.send(); // Corrected this line by adding parentheses to call the send function
